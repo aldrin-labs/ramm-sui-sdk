4a0f3ae4a2afa26fdd0accd8efba3b18891ca4f8
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2024-01-22 15:02:31 +0000

    Sign and execute liquidity deposit tx in SDK test

--- tests/liquidityDeposit.test.ts
+++ tests/liquidityDeposit.test.ts
@@ -2,7 +2,8 @@ import { suiConfigs } from "../src/constants";
 import { RAMMSuiPoolConfig, RAMMSuiPool } from "../src/types";
 import { testKeypair } from "./config";
 
-import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
+import { SUI_CLOCK_OBJECT_ID } from '@mysten/sui.js/utils';
+import { getFullnodeUrl, PaginatedCoins, SuiClient } from '@mysten/sui.js/client';
 import { getFaucetHost, requestSuiFromFaucetV1 } from '@mysten/sui.js/faucet';
 import { Secp256r1Keypair } from '@mysten/sui.js/keypairs/secp256r1';
 import { TransactionBlock, TransactionObjectArgument } from '@mysten/sui.js/transactions';
@@ -11,9 +12,9 @@ import {describe, expect, test} from 'vitest';
 
 const TESTNET: "testnet" | "devnet" | "localnet" = 'testnet';
 
-const RAMM_MISC_PKG_ID = '0x303e80a2d9dc0a2cbb6fc612d1f5cdc250420ea9e87488232f3ebc5d27e645d6';
+const RAMM_MISC_PKG_ID = '0x937e867b32da5c423e615d03d9f5e898fdf08d8f94d8b0d97805d5c3f06e0a1b';
 
-const TOKEN_FAUCET_ID = `0xce841b74ed5a0fb846834e6d876694abed03f9b9b0c838c5732e2634883d9f9b`;
+const TOKEN_FAUCET_ID = `0x61fc830d05a3f0f7fee6d601cd88023fe5d39d52dc8028e8530bf60f46d8f784`;
 
 const sleep = (ms: number) => {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -37,7 +38,7 @@ describe('Liquidity deposit', () => {
         }
 
         const poolConfig = suiTestnet[0];
-        const ramm: RAMMSuiPool = new RAMMSuiPool(poolConfig, suiClient);
+        const ramm: RAMMSuiPool = new RAMMSuiPool(poolConfig);
 
         console.log(ramm);
 
@@ -45,11 +46,11 @@ describe('Liquidity deposit', () => {
          * Request SUI from the testnet's faucet.
         */
 
-        await requestSuiFromFaucetV1({
+/*         await requestSuiFromFaucetV1({
             host: getFaucetHost(TESTNET),
             recipient: testKeypair.toSuiAddress(),
         });
-
+ */
         // Wait for the network to register the SUI faucet transaction.
         await sleep(600);
 
@@ -58,17 +59,43 @@ describe('Liquidity deposit', () => {
         */
 
         let txb = new TransactionBlock();
+        let btc_type: string = `${RAMM_MISC_PKG_ID}::test_coins::BTC`;
 
-        // BTC has 8 decimal places, so this is 1 BTC.
-        let btc_amount: number = 100_000_000;
+        // BTC has 8 decimal places, so this is 0.01 BTC.
+        let btc_amount: number = 1_000_000;
         txb.moveCall({
             target: `${RAMM_MISC_PKG_ID}::test_coin_faucet::mint_test_coins`,
             arguments: [txb.object(TOKEN_FAUCET_ID), txb.pure(btc_amount)],
-            typeArguments: [`${RAMM_MISC_PKG_ID}::test_coins::BTC`]
+            typeArguments: [btc_type]
         });
 
-        let response = await ramm.suiClient.signAndExecuteTransactionBlock({signer: testKeypair, transactionBlock: txb});
+        //await suiClient.signAndExecuteTransactionBlock({signer: testKeypair, transactionBlock: txb});
 
-        console.log(response);
+        // Wait for the network to register the test token request.
+        await sleep(1000);
+
+        /**
+         * Perform the liquidity deposit using the SDK
+         */
+
+        let paginatedCoins = await suiClient.getCoins({
+            owner: testKeypair.toSuiAddress(),
+            coinType: btc_type,
+        });
+        //console.log(paginatedCoins);
+
+        // Choose any of the test BTC coins the wallet may already have.
+        let btcId = paginatedCoins.data[0].coinObjectId;
+
+        let liqDepTxb = ramm.liquidityDeposit({
+            assetIn: btc_type,
+            globalClock: SUI_CLOCK_OBJECT_ID,
+            amountIn: btcId
+        });
+
+        console.log(liqDepTxb.blockData.transactions);
+
+        let resp = await suiClient.signAndExecuteTransactionBlock({signer: testKeypair, transactionBlock: liqDepTxb});
+        console.log('Events from test token faucet tx\'s response: ' + resp.events);
     });
 });
